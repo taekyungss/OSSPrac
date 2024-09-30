6180acc93678023cd6fd264a32c6dee5667fd6b3
taekyungss <wendy1080@naver.com>
2024-09-30 09:48:29 +0900

    2024-09-30 개인과제5 2번

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 name = input("이름을 입력하세요: ")
 student_id = input('학번을 입력하세요: ')
+Department = input('학과를 입력하세요: ')
 
 print('<출력>')
 print(f'이름 :  {name}')
 print(f'학번 : {student_id}')
+print(f'학과 : {Department}')
